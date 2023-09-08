861c928f5fa5de8ecddc957f25769fbf9d4d964c
Thanh1203 <ducthanh1122003@gmail.com>
2023-09-08 15:52:20 +0700

    Thanh base 8/9

---
+++ buy_ticket_cinema/src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+//*base css
+// import "./CSS/base.css";
+//* responsive css
+// import "./CSS/responsive.css";
+//* bootstrap css
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+//* icon
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+//*store
+import { store } from "./store/store";
+
+const app = createApp(App);
+library.add(fas);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.use(store);
+app.mount("#app");
---
+++ buy_ticket_cinema/src/store/store.js
@@ -0,0 +1,4 @@
+import { createStore } from "vuex";
+// import dataStrorage from "./dataStrorage";
+
+export const store = createStore({});
