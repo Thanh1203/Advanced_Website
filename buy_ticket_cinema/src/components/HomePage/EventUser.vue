605460edb1ce302c260b2c9c21c0373c8df34e9e
Thanh1203 <ducthanh1122003@gmail.com>
2023-09-13 21:54:42 +0700

    13/9

--- buy_ticket_cinema/src/components/HomePage/EventUser.vue
+++ buy_ticket_cinema/src/components/HomePage/EventUser.vue
@@ -2,58 +2,41 @@
     <div id="event">
         <h1>Event</h1>
         <div class="event_container">
-            <div class="event_group">
-                <div :class="`event_item${index}`" v-for="(item, index) in eventPhoto1" :key="index">
-                    <img :src="require(`@/assets/${item.img}`)" alt="">
-                </div>
-            </div>
-            <div class="event_group2"></div>
-            <div :class="`event_item${index}`" v-for="(item, index) in eventPhoto2" :key="index">
+            <div v-for="(item, index) in eventPhoto" :key="index" :class="`event${index}`">
                 <img :src="require(`@/assets/${item.img}`)" alt="">
             </div>
         </div>
     </div>
 </template>
 <script>
-const eventPhoto1 = [
+const eventPhoto = [
     {
        img:'event1.jpg' 
     },
     {
-        img: 'event4.jpg'
-    },
-    // {
-    //     img: 'event3.jpg'
-    // },
-    // {
-    //    img:'event4.jpg' 
-    // },
-    // {
-    //     img: 'event5.jpg'
-    // },
-    // {
-    //     img: 'event6.jpg'
-    // },
-    // {
-    //     img: 'event7.jpg'
-    // },
-]
-
-const eventPhoto2 = [
-    {
         img: 'event2.jpg'
     },
     {
+        img: 'event3.jpg'
+    },
+    {
+       img:'event4.jpg' 
+    },
+    {
         img: 'event5.jpg'
-    }
+    },
+    {
+        img: 'event6.jpg'
+    },
+    {
+        img: 'event7.jpg'
+    },
 ]
-
 
 export default {
     data() {
         return {
-            eventPhoto1,
-            eventPhoto2
+            eventPhoto,
         }
     }
 }
@@ -69,27 +52,12 @@ export default {
 }
 
 .event_container {
+    width: 1275px;
+    height: 1500px;
+    display: grid;
+    grid-template-columns: 325px 325px 325px;
+    grid-gap: 10px;
     margin: 0 auto;
-    width: 980px;
-    border: 1px solid red;
-}
-
-.event_group {
-    display: flex;
-    flex-direction: column;
-}
-.event_group2 {
-    display: flex;
-    flex-direction: column;
-}
-
-.event_item0 {
-    width: 315px;
-}
-
-.event_item1 {
-    width: 315px;
-    height: 164px;
 }
 
 </style>
